2a7acebe37f4f41fe4182089cee72160ec8e703d
Jeramie Hendricks <bassnsnare@gmail.com>
2018-03-22 09:21:16 -0500

    first commit

---
+++ stickyfooter.js
@@ -0,0 +1,35 @@
+function StickyFooter( args ) {
+    this.footer = null;
+
+    for ( var props in args ) {
+        if ( this.hasOwnProperty( props ) ) {
+            this[props] = args[props];
+        }
+    }
+
+    this.pos = footer.getBoundingClientRect();
+    this.height = window.innerHeight;
+    this.height = this.height - this.pos.top;
+    this.height = this.height - this.footer.offsetHeight;
+}
+
+StickyFooter.prototype = ( {
+    constructor : StickyFooter,
+    logIt : function ( e ) {
+        console.log( e.type );
+    },
+    positionFooter : function () {
+        this.footer.style.marginTop = this.height + 'px';
+    }
+} );
+
+var StickTheFooter = new StickyFooter(
+        {
+            footer : document.getElementById( "footer-container" )
+        }
+);
+
+
+[ 'load', 'resize', 'orientationChange' ].map( function ( e ) {
+    window.addEventListener( e, StickTheFooter.positionFooter.bind( StickTheFooter ) );
+} );
